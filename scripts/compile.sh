918baccc2889f9b69328801244b9844a675a8302
Siddhant <me@madebysid.com>
2019-01-08 15:40:09 +0100

    Show a spinner when TypeScript compiler is working

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+spin()
+{
+  spinner="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+  while :
+  do
+    for i in `seq 0 7`
+    do
+      echo -en "\010\010\010\010\010\010\010\010\010"
+      echo -n "${spinner:$i:1} Working"
+      sleep 0.03
+    done
+  done
+}
+
 clear
-echo "Working"
+spin &
+SPIN_PID=$!
+trap "kill -9 $SPIN_PID" `seq 0 15`
+
 tsc
 cp src/index.html dist
-clear
-echo "Finished"
+
+echo -en "\010\010\010\010\010\010\010\010\010"
+echo "✔ Finished"
