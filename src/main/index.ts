01f1cbb6abb9f235daff2e70c78d25e895005346
Siddhant <me@madebysid.com>
2019-01-10 12:35:26 +0100

    Auto open app at login

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,14 @@
-import { app, Tray } from "electron";
+import { app } from "electron";
 
+import { isDev } from "./utils";
 import { createTray } from "./tray";
 
 export default class MainProcess {
   constructor() {
+    if (!isDev()) {
+      app.setLoginItemSettings({ openAtLogin: true });
+    }
+
     app.on("ready", this.onAppReady);
   }
 
