48086cf915b06116ec33e47664e57ca681158e34
Siddhant <me@madebysid.com>
2019-01-14 20:52:35 +0100

    Organize renderer code

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,4 +1,5 @@
-import { ipcRenderer } from "electron";
+import DateService from "./services/DateService";
+import { getPreferences, setPreferences, isDirty } from "./utils";
 
 const TIME_DISPLAY_ID = "#time";
 const DOB_DAY_INPUT_ID = "#dob__day";
@@ -7,76 +8,102 @@ const DOB_YEAR_INPUT_ID = "#dob__year";
 const SAVE_BUTTON_ID = "#save";
 
 (async () => {
-  const getPreferences = async (): Promise<Preferences> => {
-    return new Promise((resolve, reject) => {
-      ipcRenderer.on("channel", (event: Event, message: IPCMessage) => {
-        return resolve(message.payload);
-      });
-
-      const message: IPCMessage = { type: "preferences-get" }
-      ipcRenderer.send("channel", message);
-    });
-  };
-
   const preferences: Preferences = await getPreferences();
-  let dayResetHour: number = preferences.dayResetHour;
-  let dayResetMinutes: number = preferences.dayResetMinutes;
-  let dob: Date = new Date(preferences.dobYear, preferences.dobMonth, preferences.dobDate);
+  let {
+    active,
+    dayResetHour,
+    dayResetMinutes,
+    dobDate,
+    dobMonth,
+    dobYear
+  } = preferences;
 
-  const handleChangeTime = (e: KeyboardEvent) => {
-    const { value } = e.target as HTMLInputElement;
-    dayResetHour = parseInt(value);
+  const updateDirty = () => {
+    if (isDirty(preferences, {
+      active,
+      dayResetHour,
+      dayResetMinutes,
+      dobDate,
+      dobMonth,
+      dobYear
+    })) {
+      saveButton.classList.remove("disabled");
+    } else {
+      saveButton.classList.add("disabled");
+    }
   }
 
-  const handleChangeDOB = (type: "day" | "month" | "year", e: KeyboardEvent) => {
-    const { value } = e.target as HTMLInputElement;
+  const handleChange = (
+    type: "dayResetHour" | "dayResetMinutes" | "dobDate" | "dobMonth" | "dobYear",
+    value: string
+  ) => {
     switch (type) {
-      case "day":
-        dob.setDate(parseInt(value));
+      case "dayResetHour":
+        dayResetHour = parseInt(value);
         break;
-      case "month":
-        dob.setMonth(parseInt(value));
+      case "dayResetMinutes":
+        dayResetMinutes = parseInt(value);
         break;
-      case "year":
-        dob.setFullYear(parseInt(value));
+      case "dobDate":
+        dobDate = parseInt(value);
+        break;
+      case "dobMonth":
+        dobMonth = parseInt(value);
+        break;
+      case "dobYear":
+        dobYear = parseInt(value);
         break;
       default:
         break;
     }
-  }
+
+    updateDirty();
+  };
 
   const handleSave = () => {
-    const message: IPCMessage = {
-      type: "preferences-set",
-      payload: {
-        dayResetHour,
-        dayResetMinutes,
-        dobDate: dob.getDate(),
-        dobMonth: dob.getMonth(),
-        dobYear: dob.getFullYear(),
-      }
-    };
-
-    ipcRenderer.send("channel", message);
+    setPreferences({
+      active,
+      dayResetHour,
+      dayResetMinutes,
+      dobDate,
+      dobMonth,
+      dobYear
+    });
   }
+
+  const getInputValue = (e: KeyboardEvent) => {
+    const { value } = e.target as HTMLInputElement;
+    return value;
+  };
 
   // Gather DOM elements
   const timeDisplay: HTMLSpanElement = document.querySelector(TIME_DISPLAY_ID);
-  const dobDayInput: HTMLInputElement = document.querySelector(DOB_DAY_INPUT_ID);
+  const dobDateInput: HTMLInputElement = document.querySelector(DOB_DAY_INPUT_ID);
   const dobMonthInput: HTMLInputElement = document.querySelector(DOB_MONTH_INPUT_ID);
   const dobYearInput: HTMLInputElement = document.querySelector(DOB_YEAR_INPUT_ID);
   const saveButton: HTMLButtonElement = document.querySelector(SAVE_BUTTON_ID);
 
   // Attach event listeners
-  timeDisplay.addEventListener("keyup", handleChangeTime);
-  dobDayInput.addEventListener("keyup", handleChangeDOB.bind(this, "date"));
-  dobMonthInput.addEventListener("keyup", handleChangeDOB.bind(this, "month"));
-  dobYearInput.addEventListener("keyup", handleChangeDOB.bind(this, "year"));
+  dobDateInput.addEventListener("keyup", (e: KeyboardEvent) => {
+    const value = getInputValue(e);
+    (Number(value) <= 31) && handleChange("dobDate", value);
+  });
+  dobMonthInput.addEventListener("keyup", (e: KeyboardEvent) => {
+    const value = getInputValue(e);
+    Number(value) <= 12 && handleChange("dobMonth", value);
+  });
+  dobYearInput.addEventListener("keyup", (e: KeyboardEvent) => {
+    const value = getInputValue(e);
+    const now = new Date();
+    Number(value) >= 1900 && Number(value) <= now.getFullYear() && handleChange("dobYear", value);
+  });
   saveButton.addEventListener("click", handleSave);
 
   // Populate initial values
-  timeDisplay.innerText = String(`${dayResetHour}:${String(dayResetMinutes).padStart(2, "0")} ${dayResetHour >= 12 ? 'PM' : 'AM'}`);
-  dobDayInput.value = String(dob.getDate());
-  dobMonthInput.value = String(dob.getMonth() + 1);
-  dobYearInput.value = String(dob.getFullYear());
+  timeDisplay.innerText = DateService.formatTime(dayResetHour, dayResetMinutes);
+  dobDateInput.value = DateService.formatDay(dobDate);
+  dobMonthInput.value = DateService.formatMonth(dobMonth);
+  dobYearInput.value = DateService.formatYear(dobYear);
+
+  updateDirty();
 })();
---
+++ src/renderer/utils.ts
@@ -0,0 +1,27 @@
+import { ipcRenderer } from "electron";
+
+export const isDirty = (source: any, modified: any) => {
+  console.log(source, modified)
+  console.log(JSON.stringify(source), JSON.stringify(modified));
+  return JSON.stringify(source) !== JSON.stringify(modified); 
+}
+
+export const setPreferences = (preferences: Preferences) => {
+  const message: IPCMessage = {
+    type: "preferences-set",
+    payload: preferences
+  };
+
+  ipcRenderer.send("channel", message);
+}
+
+export const getPreferences = async (): Promise<Preferences> => {
+  return new Promise(resolve => {
+    ipcRenderer.on("channel", (event: Event, message: IPCMessage) => {
+      return resolve(message.payload);
+    });
+
+    const message: IPCMessage = { type: "preferences-get" }
+    ipcRenderer.send("channel", message);
+  });
+};
