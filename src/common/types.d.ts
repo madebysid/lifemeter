fe34a56007fa3048bc90dc8c99408d04f80e81ca
Siddhant <me@madebysid.com>
2019-01-14 17:11:35 +0100

    Define prefs better (as a type)

--- src/common/types.d.ts
+++ src/common/types.d.ts
@@ -1,6 +1,14 @@
 declare type IPCMessage = {
-  type: string,
+  type: string
   payload: any
 }
 
 declare type ProgressType = "day" | "week" | "month" | "year" | "lifetime";
+
+declare type Preferences = {
+  active: ProgressType
+  hour: number
+  dobDate: number
+  dobMonth: number
+  dobYear: number
+};
